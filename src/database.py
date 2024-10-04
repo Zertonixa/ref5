ccff9bd9cd1036ee211fa43b9f2c579e023be4c1
nanoCAT <nanocatr@yandex.ru>
2024-10-04 17:38:45 +0300

    chore: fix

--- src/database.py
+++ src/database.py
@@ -3,8 +3,7 @@ import os
 
 from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 from dotenv import load_dotenv
 load_dotenv()
--- src/main.py
+++ src/main.py
@@ -6,5 +6,5 @@ from breed import router as breen_router
 load_dotenv()
 app = FastAPI()
 
-app.include_router(kitten_router.router, prefix="/kitten", tags=["Котёнок"])
 app.include_router(breen_router.router, prefix="/breed", tags=["Порода"])
+app.include_router(kitten_router.router, prefix="/kitten", tags=["Котёнок"])
